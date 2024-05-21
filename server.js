508401e36751677d910140bf9ead45697c7d16b2
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-21 14:42:53 +0200

    feat: Initial server with createServer and listen methods

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+const http = require('http');
 require('dotenv').config();
 
 //env variables (port and host)
-const port = process.env.PORT;
-const host = process.env.HOST;
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || "localhost";
 
-console.log(`Host: ${host} | Port: ${port}`);
+//create server function
+http
+    .createServer((req, res) => {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end("Hello World");
+    }).listen(port, host, () => {
+        const serverUrl = `http://${host}:${port}`;
+        console.log(`Server avviato su ${serverUrl}`);
+    })
