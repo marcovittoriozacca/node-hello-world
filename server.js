e7a49dcdd0d031df38398ac5860176d249128949
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-21 15:04:57 +0200

    feat: random quote from static array

--- server.js
+++ server.js
@@ -5,12 +5,23 @@ require('dotenv').config();
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "localhost";
 
+const randomQuote = () => {
+    process.argv.splice(0);
+    const randomQuoteArray = ['quote1', 'quote2', 'quote3', 'quote4'];
+    const randomIndex = Math.floor(Math.random() * randomQuoteArray.length);
+    return randomQuoteArray[randomIndex];
+}
+
 //create server function
 http
     .createServer((req, res) => {
+        
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.end("Hello World");
+        const quote = randomQuote();
+        res.end(`<h1>${quote}</h1>`);
+
     }).listen(port, host, () => {
         const serverUrl = `http://${host}:${port}`;
         console.log(`Server avviato su ${serverUrl}`);
+
     })
